951ff7cefb1259e9ba487082783a4b63b02ac222
rua-iri <rory.mcguigan@protonmail.com>
2023-03-04 14:22:10 +0000

    add struct as return value so that the units for file sizes vary according to how big they are

--- filesize.c
+++ filesize.c
@@ -6,7 +6,12 @@
 // function prototypes
 int checkCmdArgs(int numArgs);
 void getDirectoryFiles(char dirPath[]);
-int getFileSize(char dirPath[], char fileName[]);
+struct sizeUnit getFileSize(char dirPath[], char fileName[]);
+
+struct sizeUnit {
+    float size;
+    char unit;
+};
 
 
 
@@ -31,18 +36,27 @@ void getDirectoryFiles(char dirPath[])
 
     DIR *directory;
     struct dirent *dir;
+    struct sizeUnit fileSizeUnit;
+    char filePrefix[] = " B";
 
     directory = opendir(dirPath);
 
     if (directory)
     {
+
+        printf("%6s\t%-6s%s\n\n", "Size", "Unit", "File Name");
+
         // iterate through each file in the directory
         while ((dir = readdir(directory)) != NULL)
         {
             // check that file is not a hidden file
             if (dir->d_name[0] != '.')
             {
-                printf("%-25s%-12d\n", dir->d_name, getFileSize(dirPath, dir->d_name));
+                fileSizeUnit = getFileSize(dirPath, dir->d_name);
+                filePrefix[0] = fileSizeUnit.unit;
+
+                printf("%5.1f\t%-6s", fileSizeUnit.size, filePrefix);
+                printf("%s\n", dir->d_name);
             }
         }
 
@@ -53,7 +67,7 @@ void getDirectoryFiles(char dirPath[])
 
 
 
-int getFileSize(char dirPath[], char fileName[])
+struct sizeUnit getFileSize(char dirPath[], char fileName[])
 {
     int fileSize;
     struct stat st;
@@ -68,7 +82,27 @@ int getFileSize(char dirPath[], char fileName[])
     stat(fullPath, &st);
     fileSize = st.st_size;
 
-    return fileSize;
+    struct sizeUnit fileSizeUnit;
+
+
+    // format the filesize depending on whether it is 
+    // larger than a certain number of bytes
+    if (fileSize>1000000000) {
+        fileSizeUnit.size = ((float) fileSize / 1000000000);
+        fileSizeUnit.unit = 'G';
+    } else if (fileSize>1000000) {
+        fileSizeUnit.size = ((float) fileSize / 1000000);
+        fileSizeUnit.unit = 'M';
+    } else if (fileSize>1000) {
+        fileSizeUnit.size = ((float) fileSize / 1000);
+        fileSizeUnit.unit = 'k';
+    } else {
+        fileSizeUnit.size = (float) fileSize;
+        fileSizeUnit.unit = ' ';
+    }
+
+
+    return fileSizeUnit;
 }
 
 
@@ -80,6 +114,10 @@ int main(int argc, char *argv[])
     // stored as a long type variable b/c it supports values up to 1x10^18
 
     // TODO check if directory exists and do some error handling if it does not
+
+    // TODO maybe call getDirectoryFiles() recursively once so that users 
+    // can see the real size of subdirectories and not 4096
+    
     
     // getDirectoryFiles("testdir");
 
