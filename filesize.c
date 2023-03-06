c7b8fe125b383c184eab0f1d8ae6169ea152c93e
rua-iri <rory.mcguigan@protonmail.com>
2023-03-06 14:09:44 +0000

    split getDirectoryFiles into two different functions

--- filesize.c
+++ filesize.c
@@ -4,9 +4,9 @@
 #include <string.h>
 
 // function prototypes
-int checkCmdArgs(int numArgs);
 int checkIsDir(char dirPath[], char fileName[]);
-int getDirectoryFiles(char dirPath[], int isFirstLevel);
+int *getDirectoryFilesHigher(char dirPath[]);
+int getDirectoryFilesLower(char dirPath[]);
 int getFileSize(char dirPath[], char fileName[]);
 struct sizeUnit formatFileSize(int fileSize);
 
@@ -15,6 +15,8 @@ struct sizeUnit
     float size;
     char unit;
 };
+
+
 
 struct sizeUnit formatFileSize(int fileSize)
 {
@@ -44,7 +46,9 @@ struct sizeUnit formatFileSize(int fileSize)
     }
 
     return fileSizeUnit;
-};
+}
+
+
 
 int checkIsDir(char dirPath[], char fileName[])
 {
@@ -63,21 +67,10 @@ int checkIsDir(char dirPath[], char fileName[])
     return S_ISDIR(st.st_mode);
 }
 
-// function to check that user has only provided one argument
-int checkCmdArgs(int numArgs)
-{
-    if (numArgs == 2)
-    {
-        return 1;
-    }
-    else
-    {
-        printf("%i Arguments Provided\t 1 Expected\n", (numArgs - 1));
-        return 0;
-    }
-}
 
-int getDirectoryFiles(char dirPath[], int isFirstLevel)
+
+
+int *getDirectoryFilesHigher(char dirPath[])
 {
 
     DIR *directory;
@@ -85,18 +78,28 @@ int getDirectoryFiles(char dirPath[], int isFirstLevel)
 
     int totalDirSize = 0;
     int fSize;
-    int isDir;
     struct sizeUnit fileSizeUnit;
     char filePrefix[] = " B";
+    int dirElementsCount = 0;
 
     directory = opendir(dirPath);
 
-    if (directory)
-    {
-        if(isFirstLevel) {
-            printf("%6s\t%-6s%s\n\n", "Size", "Unit", "File Name");
+    if (directory) {
+
+        printf("%6s\t%-6s%s\n\n", "Size", "Unit", "File Name");
+    
+        // count the number of files in the directory
+        while ((dir = readdir(directory)) != NULL) {
+            if(dir->d_name[0]!='.') {
+                dirElementsCount++;
+            }
         }
 
+        int fileSizeAra[dirElementsCount];
+
+        // TODO remove this (just for testing)
+        // printf("\n\nTotal Directory elements: %d\n\n", dirElementsCount);
+        rewinddir(directory);
 
         // iterate through each file in the directory
         while ((dir = readdir(directory)) != NULL)
@@ -105,42 +108,81 @@ int getDirectoryFiles(char dirPath[], int isFirstLevel)
             if (dir->d_name[0] != '.')
             {
 
-                isDir = checkIsDir(dirPath, dir->d_name);
 
-                // TODO if isDir then call this function recursively
-
-                if (isDir) {
+                // call function recursively if the file is a directory
+                if (checkIsDir(dirPath, dir->d_name)) {
                     char fullPath[strlen(dirPath)+strlen(dir->d_name) + 1];
                     strcpy(fullPath, dirPath); strcat(fullPath, "/"); strcat(fullPath, dir->d_name);
-                    fSize = getDirectoryFiles(fullPath, 0);
+                    fSize = getDirectoryFilesLower(fullPath);
+                    totalDirSize += 4096;
                 } else {
                     fSize = getFileSize(dirPath, dir->d_name);
                 }
 
-                    totalDirSize += fSize;
+                totalDirSize += fSize;
 
-                if(isFirstLevel) {
-                    fileSizeUnit = formatFileSize(fSize);
-                    filePrefix[0] = fileSizeUnit.unit;
-                    printf("%5.1f\t%-6s", fileSizeUnit.size, filePrefix);
-                    printf("%s\n", dir->d_name);
-                }
+                fileSizeUnit = formatFileSize(fSize);
+                filePrefix[0] = fileSizeUnit.unit;
+                printf("%5.1f\t%-6s", fileSizeUnit.size, filePrefix);
+                printf("%s\n", dir->d_name);
+
+                fileSizeAra[dirElementsCount] = fSize;
+                dirElementsCount--;
             }
         }
 
-        if(isFirstLevel) {
-            struct sizeUnit totalFileSizeUnit = formatFileSize(totalDirSize);
-            filePrefix[0] = totalFileSizeUnit.unit;
-            printf("\n%5.1f\t%-6s", totalFileSizeUnit.size, filePrefix);
-            printf("Total\n\n");
-        }
 
 
         closedir(directory);
+
+        struct sizeUnit totalFileSizeUnit = formatFileSize(totalDirSize);
+        filePrefix[0] = totalFileSizeUnit.unit;
+        printf("\n%5.1f\t%-6s", totalFileSizeUnit.size, filePrefix);
+        printf("Total\n\n");
+
+        // TODO this seems to cause an overflow for the root directory
+        // because ints are not large enough to hold this value
+        return fileSizeAra;
     }
 
+}
+
+
+
+int getDirectoryFilesLower(char dirPath[]) {
+
+    DIR *directory;
+    struct dirent *dir;
+    int totalDirSize = 0;
+    int fSize;
+
+    directory = opendir(dirPath);
+
+    if (directory) {
+        while ((dir = readdir(directory)) != NULL) {
+
+            // check that file is not a hidden file (we will ignore those)
+            if (dir->d_name[0] != '.') {
+                // call function recursively if the file is a directory
+                if (checkIsDir(dirPath, dir->d_name)) {
+                    char fullPath[strlen(dirPath)+strlen(dir->d_name) + 1];
+                    strcpy(fullPath, dirPath); strcat(fullPath, "/"); strcat(fullPath, dir->d_name);
+                    fSize = getDirectoryFilesLower(fullPath);
+                    totalDirSize += 4096;
+                } else {
+                    fSize = getFileSize(dirPath, dir->d_name);
+                }
+
+                totalDirSize += fSize;
+            }
+        }
+        closedir(directory);
+    }
     return totalDirSize;
 }
+
+
+
 
 int getFileSize(char dirPath[], char fileName[])
 {
@@ -158,23 +200,27 @@ int getFileSize(char dirPath[], char fileName[])
     return fileSize;
 }
 
+
+
+
 int main(int argc, char *argv[])
 {
 
     // TODO check if directory exists and do some error handling if it does not
 
-    // TODO maybe call getDirectoryFiles() recursively so that users
-    // can see the real size of subdirectories and not 4096
-    // but do so only once!!
-
-    // getDirectoryFiles("testdir");
+    // TODO check that argument passed in is a directory
 
     // ensure that the right arguments have been provided
-    if (checkCmdArgs(argc) == 1)
-    {
+    if (argc == 2) {
         printf("Files in %s:\n\n", argv[1]);
-        getDirectoryFiles(argv[1], 1);
+        int *fileSizeAra = getDirectoryFilesHigher(argv[1]);
+
+
+    } else {
+        printf("%i Arguments Provided\t 1 Expected\n", (argc - 1));
+
     }
+
 
     return 0;
 }
